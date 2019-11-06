7a9cb20c1e07b6fff536e9695f3b7c269ee44167
trusov <trusov@haulmont.com>
2019-11-07 02:01:00 +0400

    with error

---
+++ src/main/java/com/example/demo/model/Country.java
@@ -0,0 +1,34 @@
+package com.example.demo.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.springframework.context.annotation.Bean;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+public class Country implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @EqualsAndHashCode.Exclude
+    private int id;
+
+    private String name;
+
+    private String codeName;
+
+    public Country(int id, String name, String codeName) {
+        this.id = id;
+        this.name = name;
+        this.codeName = codeName;
+    }
+
+    public Country(String name, String codeName) {
+        this.name = name;
+        this.codeName = codeName;
+    }
+
+}
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,83 @@
 package com.example.demo;
 
+import com.example.demo.dao.CountryDao;
+import com.example.demo.dao.CountryNotFoundException;
+import com.example.demo.model.Country;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.test.annotation.DirtiesContext;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
 
 @SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    protected EmbeddedDatabase db;
 
+    @Autowired
+    protected CountryDao countryDao;
+
+    private List<Country> expectedCountryList = new ArrayList<>();
+    private List<Country> expectedCountryListStartsWithA = new ArrayList<>();
+    private Country countryWithChangedName = new Country(1, "Russia", "RU");
+
+    @BeforeAll
+    public void setUp()  {
+        db = (EmbeddedDatabase) countryDao.dataSource();
+        initExpectedCountryLists();
+        countryDao.loadCountries();
+    }
+
+    @Test
+    public void testCountryList() throws InterruptedException, CountryNotFoundException {
+        List<Country> countryList = countryDao.getCountryList();
+        assertNotNull(countryList);
+        assertEquals(expectedCountryList.size(), countryList.size());
+        for (int i = 0; i < expectedCountryList.size(); i++) {
+            assertEquals(expectedCountryList.get(i), countryList.get(i));
+        }
+
+//        org.hsqldb.util.DatabaseManager.main(new String[]{});
+//        Thread.sleep(90000);
+    }
+
+    @Test
+    public void testCountryListStartsWithA() {
+        List<Country> countryList = countryDao.getCountryListStartWith("A");
+        assertNotNull(countryList);
+        assertEquals(expectedCountryListStartsWithA.size(), countryList.size());
+        for (int i = 0; i < expectedCountryListStartsWithA.size(); i++) {
+            assertEquals(expectedCountryListStartsWithA.get(i), countryList.get(i));
+        }
+    }
+
+    @Test
+    public void testCountryChange()  {
+        countryDao.updateCountryName("RU", "Russia");
+        assertEquals(countryWithChangedName, countryDao.getCountryByCodeName("RU"));
+    }
+
+    private void initExpectedCountryLists() {
+        for (int i = 0; i < CountryDao.COUNTRY_INIT_DATA.length; i++) {
+            String[] countryInitData = CountryDao.COUNTRY_INIT_DATA[i];
+            Country country = new Country(i, countryInitData[0], countryInitData[1]);
+            expectedCountryList.add(country);
+            if (country.getName().startsWith("A")) {
+                expectedCountryListStartsWithA.add(country);
+            }
+        }
+    }
 }
